5c9a7e09be33b2610af16a83d6f6b3d81e19c695
DiYaZeN <61739248+DiYaZeN@users.noreply.github.com>
2024-08-08 18:20:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,9 @@
-#!/bin/bash 
-
+#!/bin/bash
 echo "===================="
-
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory "/github/workspace"
-
+. /opt/venv/bin/activate
 python3 /usr/bin/feed.py
-
-git add -A && git commit -m "Update feed" 
-
-git push --set-upstream origin master
-
-
+git add -A && git commit -m "Update feed"
+git push --set-upstream origin main
